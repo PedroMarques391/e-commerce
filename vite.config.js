be74759974f14319f92101f2feaacf37c08d29da
Pedro <pedromarques391@gmail.com>
2023-12-11 16:13:43 -0300

    Ajuste de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/MarquesMagazine/',
+  base: '/e-commerce/',
   plugins: [],
   resolve: {
     /* something */
